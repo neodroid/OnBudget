d7a4c867ca12f631f1396c05af4aff1db409b4d8
vcentsu <vincenttius.sutanto@gmail.com>
2022-04-07 17:08:06 +0700

    added Outlet and Action AddTrip

--- OnBudget/ViewController.swift
+++ OnBudget/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addTripBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressAddTrip(_ sender: Any) {
+        print("create ttrip")
+    }
 }
 
