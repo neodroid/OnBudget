6a3f046438469509d6539a17a4e94a4f077cc713
neodroid <kevinahmad.f@gmail.com>
2022-04-07 19:24:11 +0700

    add MVC (Model View Controller) and Trip struct

---
+++ OnBudget/Model/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  Trip.swift
+//  OnBudget
+//
+//  Created by Kevin ahmad on 07/04/22.
+//
+
+import Foundation
+
+
+struct Trip {
+    let name: String
+    let destination: String
+    let budget: Float
+    
+    // start date
+    // end date
+    
+}
