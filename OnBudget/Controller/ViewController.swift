8818697c774f0510e2786554305c47b7d1e3bd76
vcentsu <vincenttius.sutanto@gmail.com>
2022-04-08 20:44:10 +0700

    Added cancel and add button in AddTripViewController

--- OnBudget/Controller/ViewController.swift
+++ OnBudget/Controller/ViewController.swift
@@ -13,18 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainBg: UIImageView!
     @IBOutlet weak var addTripBtn: UIButton!
     
+    public var currTripStatus : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Check if currTripStatus true
+        if currTripStatus{
+            mainBg.isHidden = true
+            
+        }
     }
 
 
     @IBAction func pressAddTrip(_ sender: Any) {
         print("added trip")
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "AddTrip") as? AddTripViewController else{
-            return
-        }
-        present(vc, animated: true)
+//        guard let vc = storyboard?.instantiateViewController(withIdentifier: "AddTrip") as? AddTripViewController else{
+//            return
+//        }
+//        present(vc, animated: true)
     }
 }
 
